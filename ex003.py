1a37a02975ee867794ee5ef2d1f8742f9fbccc50
David O.Silva <david2015os@hotmail.com>
2021-11-11 20:46:57 -0300

    Exercicio 003

---
+++ ex003.py
@@ -0,0 +1,10 @@
+'''
+Crie um programa que leia dois números e mostre a soma entre eles.
+'''
+
+firstNumber = int(input('Insira um número: '))
+secondNumber = int(input('Insira outro número: '))
+
+sum = firstNumber + secondNumber
+
+print('A soma entre {} + {} = {}' .format(firstNumber, secondNumber, sum))
