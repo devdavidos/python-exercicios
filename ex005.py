4b9da0708672bc6111c368e4244caa9ac6cf2cd4
David O.Silva <david2015os@hotmail.com>
2021-11-12 17:56:18 -0300

    Exercicio 005

---
+++ ex005.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+'''
+number = int(input('Digite um número: '))
+
+print('O número é: {}, seu sucessor: {}, seu antecessor: {}' .format(number, number + 1, number - 1))
+
+### Abreviação number++, number-- 
